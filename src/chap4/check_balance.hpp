a698fa2d3eb2e4cc766b1c592aa6cd0b27e424ad
Sharkman <sharkman.jw@gmail.com>
2012-04-08 16:35:18 -0400

    finished is_tree_balanced

--- src/chap4/check_balance.hpp
+++ src/chap4/check_balance.hpp
@@ -5,29 +5,29 @@
 
 // Return tree's height if tree is balanced; return -1 if not balanced.
 template<typename T>
-int check_tree_balance_and_height(typename ww::BinarySearchTree<T>::Node* root)
+int check_tree_balance_and_height(typename ww::BinaryTree<T>::Node* root)
 {
     if (NULL == root)
         return 0;
         
-    int left_height = check_tree_balance_and_height(root->left);
+    int left_height = check_tree_balance_and_height<T>(root->left);
     if (left_height == -1)
         return -1;
         
-    int right_height = check_tree_balance_and_height(root->right);
+    int right_height = check_tree_balance_and_height<T>(root->right);
     if (right_height == -1)
         return -1;
     
     int height_diff = left_height - right_height;
     if (std::abs(height_diff) > 1)
         return -1;
-    return height_diff > 0 ? left_height : right_height;
+    return (height_diff > 0 ? left_height : right_height) + 1;
 }
 
 template<typename T>
-bool is_tree_balanced(typename ww::BinarySearchTree<T>::Node* root)
+bool is_tree_balanced(typename ww::BinaryTree<T>::Node* root)
 {
-    return check_tree_balance_and_height(root) != -1;
+    return check_tree_balance_and_height<T>(root) != -1;
 }
 
 
