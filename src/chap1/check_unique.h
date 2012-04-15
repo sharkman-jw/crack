493a640d6ed1bdb2a8b537822b520de19e195bde
Sharkman <sharkman.jw@gmail.com>
2012-04-15 01:06:58 -0400

    added problem descriptions

--- src/chap1/check_unique.h
+++ src/chap1/check_unique.h
@@ -1,3 +1,8 @@
+// 1.1
+// Implement an algorithm to determine if a string has all unique characters.
+// What if you can not use additional data structures?
+//
+
 #ifndef _CHAP1_1_UNIQUE_CHARS_H_
 #define _CHAP1_1_UNIQUE_CHARS_H_
 
--- src/chap1/reverse_str.h
+++ src/chap1/reverse_str.h
@@ -1,3 +1,7 @@
+// 1.2
+// Write code to reverse a C-Style String (C-String means that “abcd” is
+// represented as five characters, including the null character )
+//
 #ifndef _CHAP1_2_REVERSE_STR_H_
 #define _CHAP1_2_REVERSE_STR_H_
 
